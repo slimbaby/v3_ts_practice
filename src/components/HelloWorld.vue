590eb8f48688ccc6eb962a3228f480feb9da593e
slimbaby <460467982@qq.com>
2024-04-28 22:13:56 +0800

    fix: vue3+ts练习仓库搭建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>1111111</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+const msg = ref("666");
+function changeMsg(str: string) {
+  msg.value = str;
+}
+onMounted(() => {
+  changeMsg("我改变了！");
+});
+</script>
+
+<style scoped lang="scss">
+.hello{
+  color: pink;
+  h1{
+    font-size: 48px;
+  }
+}
+</style>
